b5e22c87ddc584515b9df5fabe16fc97ae57e682
Zagoshipda <john713@hanmail.net>
2019-12-04 23:02:24 +0900

    feat: Project5 4. Sensor Upgrade

---
+++ 5_MappingAndSLAM/Project5_MapMyWorld/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/4_Localization/Project4_WhereAmI/catkin_ws/src
+# Build directory: /home/workspace/4_Localization/Project4_WhereAmI/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(my_robot)
+subdirs(teleop_twist_keyboard)
+subdirs(ball_chaser)
+subdirs(pgm_map_creator)
