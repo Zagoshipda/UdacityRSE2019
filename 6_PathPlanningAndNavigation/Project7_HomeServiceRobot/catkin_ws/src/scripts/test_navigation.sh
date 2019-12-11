fdc4739e3e06d287e366aa22b7cf5b10a77c30c1
JeonHyeong Lee <john713@hanmail.net>
2019-12-11 15:09:44 +0000

    feat: Project7 6. Localization and Navigation Testing [DONE]

--- 6_PathPlanningAndNavigation/Project7_HomeServiceRobot/catkin_ws/src/scripts/test_navigation.sh
+++ 6_PathPlanningAndNavigation/Project7_HomeServiceRobot/catkin_ws/src/scripts/test_navigation.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " & 
+export ROBOT_INITIAL_POSE="-x 0 -y 0 -Y 1.5708"
+
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/6_PathPlanningAndNavigation/Project7_HomeServiceRobot/catkin_ws/src/my_robot/worlds/UdacityOffice.world " &
 sleep 5
-xterm  -e  " roslaunch turtlebot_gazebo  amcl.launch " & 
+
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/6_PathPlanningAndNavigation/Project7_HomeServiceRobot/catkin_ws/src/my_robot/maps/map.yaml " &
 sleep 5
-xterm  -e  " roslaunch turtlebot_viz_launchers view_navigation.launch "
+
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch "
--- 6_PathPlanningAndNavigation/Project7_HomeServiceRobot/catkin_ws/src/scripts/test_slam.sh
+++ 6_PathPlanningAndNavigation/Project7_HomeServiceRobot/catkin_ws/src/scripts/test_slam.sh
@@ -6,7 +6,7 @@ sleep 5
 xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch " &
 sleep 5
 
-xterm -e " roslaunch turtlebot_viz_launchers view_navigation.launch " &
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
 
 xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch "
