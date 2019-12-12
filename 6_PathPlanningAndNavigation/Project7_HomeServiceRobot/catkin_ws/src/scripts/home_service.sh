15962807dd52fe5a3df66619fb2d9dc0e04506a4
JeonHyeong Lee <john713@hanmail.net>
2019-12-12 04:45:23 +0000

    feat: Project7 9. Your Home Service Robot

---
+++ 6_PathPlanningAndNavigation/Project7_HomeServiceRobot/catkin_ws/src/scripts/home_service.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+export ROBOT_INITIAL_POSE="-x 0 -y 0 -Y 1.5708"
+
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/6_PathPlanningAndNavigation/Project7_HomeServiceRobot/catkin_ws/src/my_robot/worlds/UdacityOffice.world " &
+sleep 5
+
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/6_PathPlanningAndNavigation/Project7_HomeServiceRobot/catkin_ws/src/my_robot/maps/map.yaml " &
+sleep 5
+
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+sleep 5
+
+xterm -e " rosrun pick_objects pick_objects " &
+sleep 5
+
+xterm -e " rosrun add_markers add_markers "
